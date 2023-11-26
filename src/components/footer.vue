d78869657b2bf2af05caadcb638a3fc7555d62b4
Jaanis <jaanis.soosaar@ut.ee>
2023-11-26 19:46:03 +0200

    Adding stuff to footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,8 @@
 <template>
     <footer>
-        <p><Strong>PostIt</Strong></p>
+        <p><strong>PostIt</strong></p>
+        <p>© 2023 PostIt. All rights reserved.</p>
+        <p><router-link to="/">Terms of Service</router-link> | <router-link to="/">Privacy Policy</router-link></p>
     </footer>
 </template>
 
@@ -14,9 +16,10 @@ footer{
     border-radius: 10px;
     background-color: #D2E3C8;
     align-items: center;
-    text-align: center;
+    text-align: left;
     padding-top: 30px;
     padding-bottom: 30px;
+    padding-left: 30px;
 
     bottom: 0;
     left: 0;
